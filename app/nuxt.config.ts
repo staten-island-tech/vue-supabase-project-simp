4bdfed3df675314a044fbabad27de34e8e82bb6a
Raymondl177 <raymondl177@nycstudents.net>
2026-04-29 11:59:44 -0400

    csc

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,6 +2,13 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+  modules: ['@pinia/nuxt'],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
