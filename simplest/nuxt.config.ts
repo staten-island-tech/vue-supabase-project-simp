2d25b2c717a15b7ae545ff8adddb00d1b115bb67
ierhe <yuningw05@gmail.com>
2026-05-20 12:08:19 -0400

    zh

--- simplest/nuxt.config.ts
+++ simplest/nuxt.config.ts
@@ -1,14 +1,5 @@
-import tailwindcss from "@tailwindcss/vite"
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css:['./app/assets/css/main.css'],
-  modules:[
-    '@pinia/nuxt',
-  ],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  vite:{
-    plugins:[
-      tailwindcss(),
-    ],
-  },
-});
+  devtools: { enabled: true }
+})
