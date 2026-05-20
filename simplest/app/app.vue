5c81aa440ba8313d6387592ea39e1ed5dbecb64a
ierhe <yuningw05@gmail.com>
2026-05-20 12:16:17 -0400

    work

--- simplest/app/app.vue
+++ simplest/app/app.vue
@@ -1,6 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { createClient } from '@supabase/supabase-js'
+
+const config = useRuntimeConfig()
+const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey)
+
+const todos = ref([])
+
+async function getTodos() {
+  const { data } = await supabase.from('todos').select()
+  todos.value = data
+}
+
+onMounted(() => {
+  getTodos()
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+  </ul>
 </template>
--- simplest/nuxt.config.ts
+++ simplest/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+  modules: ['@pinia/nuxt',
+    '@nuxtjs/supabase'],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
+
