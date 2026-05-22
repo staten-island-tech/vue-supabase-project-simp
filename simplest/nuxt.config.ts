a7df8194eb8afd6e58b83a15213e45ae72407f9a
ierhe <yuningw05@gmail.com>
2026-05-22 12:29:02 -0400

    asd

--- simplest/nuxt.config.ts
+++ simplest/nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  modules: ['@nuxtjs/supabase',/*'@pinia/nuxt'*/],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
