a806af5a6bdca49dcc02e775bf0dc167f9b1266d
ierhe <yuningw05@gmail.com>
2026-05-26 12:22:54 -0400

    Update nuxt.config.ts

--- simplest/nuxt.config.ts
+++ simplest/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
-  modules: ['@nuxtjs/supabase',/*'@pinia/nuxt'*/],
+  modules: ['@nuxtjs/supabase',/* '@pinia/nuxt' */],
   vite: {
     plugins: [
       tailwindcss(),
